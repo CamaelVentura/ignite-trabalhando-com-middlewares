c082c16d8167dfccacbce60a53c0e163cc4373d0
Camael Ventura <camael.ventura@gmail.com>
2023-03-18 17:38:28 -0300

    checking if user can create a todo

--- src/index.js
+++ src/index.js
@@ -25,7 +25,14 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length === 10) {
+
+    return response.status(403).json({ error: "User already reach max todos"});
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
